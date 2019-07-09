4a9f3cad6a57d30871ef0fbffc72d5aaddb8581a
JasonAHeron <jasonaheron@gmail.com>
2019-07-09 16:29:22 -0700

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule , ReactiveFormsModule} from '@angular/forms';
 import { CvaWrapperComponent } from './cva-wrapper.component';
 import { TemplateFormComponent } from './template-form.component';
 import { CvaConsumerComponent } from './cva-consumer.component';
-import { MyAsyncValidator } from './asyncValidator';
+import { SquareImageValidator , ImageLoaderService} from './asyncValidator';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
-  declarations: [ CvaWrapperComponent, TemplateFormComponent, CvaConsumerComponent, MyAsyncValidator ],
-  bootstrap:    [ CvaConsumerComponent ]
+  declarations: [ CvaWrapperComponent, TemplateFormComponent, CvaConsumerComponent, SquareImageValidator ],
+  bootstrap:    [ CvaConsumerComponent ],
+  providers: [ImageLoaderService],
 })
 export class AppModule { }
--- src/app/asyncValidator.ts
+++ src/app/asyncValidator.ts
@@ -1,19 +1,65 @@
-import { Directive } from '@angular/core';
-import { AbstractControl, AsyncValidator, NG_ASYNC_VALIDATORS, ValidationErrors } from '@angular/forms';
-import { Observable, of, } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import {Directive, Injectable} from '@angular/core';
+import {AbstractControl, AsyncValidator, NG_ASYNC_VALIDATORS, ValidationErrors} from '@angular/forms';
+import {Observable, of, Subject} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
 
+/** Image Dimesions */
+export interface Dimensions {
+  width: number;
+  height: number;
+}
+
+/** An angular service that returns image dimensions from an url */
+@Injectable()
+export class ImageLoaderService {
+  /*
+   * A method that loads an image from a url & returns its dimensions as an
+   * observable
+   */
+  loadImageDimensions(url: string): Observable<Dimensions> {
+    const img = new Image();
+    const subject = new Subject<Dimensions>();
+    img.onload = () => {
+      const {width, height} = img;
+      subject.next({width, height});
+      subject.complete();
+    };
+
+    img.onerror = (err) => {
+      subject.error(err);
+    };
+    img.src=url;
+    return subject;
+  }
+}
+
+
+/**
+ * A validator to validate whether has square-like dimensions
+ */
 @Directive({
-  selector: '[asyncValidator][ngModel]',
+  selector: '[validSquareImage][ngModel]',
   providers: [{
     provide: NG_ASYNC_VALIDATORS,
-    useExisting: MyAsyncValidator,
+    useExisting: SquareImageValidator,
     multi: true,
   }]
 })
-export class MyAsyncValidator implements AsyncValidator {
+export class SquareImageValidator implements AsyncValidator {
+  constructor(private readonly loader: ImageLoaderService) {}
 
-  validate(control: AbstractControl): Observable<ValidationErrors | null> {
-    return of(null).pipe(delay(2000));
+  /** @override */
+  validate(control: AbstractControl): Observable<ValidationErrors|null> {
+    const url = control.value;
+    if (!url) return of(null);
+    return this.loader.loadImageDimensions(url).pipe(
+        map(({width, height}: Dimensions) => {
+          const ratio = width / height;
+          if (ratio > 1.1 || ratio < 0.9) {  // relax the bound slightly
+            return {imgNotSquare: ratio};
+          }
+          return null;
+        }),
+        catchError((err) => of(null)));
   }
 }
